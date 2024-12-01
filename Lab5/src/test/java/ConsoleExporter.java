529260da5f90837b408b222995c0cd3d1d76c3b6
Xuantruong151004 <xuantruong151004@gmail.com>
2024-12-01 15:47:41 +0700

    Delete Lab5 directory

--- Lab5/src/test/java/ConsoleExporter.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public class ConsoleExporter extends  ResultExporter {
-    public void export(List<String> results) {
-        results.forEach(System.out::println);
-    }
-}
--- Lab5/src/test/java/TransactionParser.java
+++
@@ -1,5 +0,0 @@
-import java.util.List;
-
-public interface TransactionParser {
-    List<BankTransaction> parse(String input) throws Exception;
-}
